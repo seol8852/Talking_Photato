be4c3d572ed2539966f02fc80a319dd4459069a0
Han Chan Hyeok <chhhanya@hs.ac.kr>
2026-04-13 19:37:56 +0900

    Update supabaseClient.js

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://nvcnhrisyurilwtkiplg.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52Y25ocmlzeXVyaWx3dGtpcGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjA4NzMsImV4cCI6MjA5MTI5Njg3M30.SiybcwaM3rLWu0V-JqxHbj0iVUNFAL-R1t6CFWU5zA4";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey)
